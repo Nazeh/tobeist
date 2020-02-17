02abfc063b314a43d8a78da00a432d31820d4acf
nazeh <ar.nazeh@gmail.com>
2020-02-17 22:35:28 +0300

    ✨ Firebase functions for create/read projects

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,40 @@
 const functions = require('firebase-functions');
+const app = require('express')();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+app.get('/projects', (req, res) => {
+  db.collection('projects')
+    .get()
+    .then(data => {
+      const projects = [];
+      data.forEach(doc => {
+        projects.push(doc.data());
+      });
+      res.json(projects);
+    })
+    .catch(err => console.log(err));
 });
+
+app.post('projects', (req, res) => {
+  const newPrject = {
+    name: req.body.name,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  db.collection('projects')
+    .add(newPrject)
+    .then(doc => {
+      res.json({ messages: `document ${doc.id} was created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: `something went wrong` });
+      console.log(err);
+    });
+});
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
