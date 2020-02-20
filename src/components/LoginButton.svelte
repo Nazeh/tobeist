0106ba52853b2bd42df95e3d199e3075d32cd711
nazeh <ar.nazeh@gmail.com>
2020-02-20 18:33:14 +0300

    💄 Updating the UI and style files.

---
+++ src/components/LoginButton.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let clickHandler;
+</script>
+
+<style>
+  button {
+    position: relative;
+    background: none;
+    height: 38px;
+    border-radius: 3px;
+    margin-bottom: 13px;
+  }
+
+  button:hover {
+    background: rgba(0, 0, 0, 0.05);
+    cursor: pointer;
+    border-color: grey;
+  }
+</style>
+
+<button on:click={clickHandler} type="button">
+  <slot />
+</button>
--- src/containers/LoginPage.svelte
+++ src/containers/LoginPage.svelte
@@ -1,21 +1,52 @@
 <script>
   import { login } from 'utils/auth';
   import Main from '../layout/Main';
-  const loginGoogle = () => login('GOOGLE');
+  import LoginButton from 'components/LoginButton';
+
+  const loginAnon = () => {
+    spinner = true;
+    return login();
+  };
+
+  const loginGoogle = () => {
+    spinner = true;
+    return login('GOOGLE');
+  };
+
+  let spinner;
 </script>
 
 <style>
-  div {
+  .login-bg {
     height: 100%;
+    background-color: #f1f1f1;
+  }
+
+  .wrapper {
+    width: 750px;
+    height: 100%;
+    max-width: 100%;
+    margin: auto;
     display: flex;
     justify-content: center;
-    background-color: #f1f1f1;
+    align-items: center;
+    background-color: white;
+  }
+
+  form {
+    width: 332px;
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
   }
 </style>
 
-<div>
-  <Main>
-    <button on:click={loginGoogle}>Signin with Google</button>
-    <button on:click={login}>Signin Anonymously</button>
-  </Main>
+<div class="login-bg">
+  <div class="wrapper">
+    <form action="">
+      <LoginButton clickHandler={loginGoogle}>Signin with Google</LoginButton>
+      <LoginButton clickHandler={loginAnon}>Signin Anonymously</LoginButton>
+    </form>
+  </div>
 </div>
