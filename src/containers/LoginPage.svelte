95ab4865221e4e4be95261abeb8a124db2399956
nazeh <ar.nazeh@gmail.com>
2020-02-20 13:45:46 +0300

    ✨ Add ThemeProvider with sass themes generator

--- src/containers/LoginPage.svelte
+++ src/containers/LoginPage.svelte
@@ -1,10 +1,21 @@
 <script>
   import { login } from 'utils/auth';
-  import Main from 'components/Main';
+  import Main from '../layout/Main';
   const loginGoogle = () => login('GOOGLE');
 </script>
 
-<Main>
-  <button on:click={loginGoogle}>Signin with Google</button>
-  <button on:click={login}>Signin Anonymously</button>
-</Main>
+<style>
+  div {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: #f1f1f1;
+  }
+</style>
+
+<div>
+  <Main>
+    <button on:click={loginGoogle}>Signin with Google</button>
+    <button on:click={login}>Signin Anonymously</button>
+  </Main>
+</div>
---
+++ src/layout/index.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Main from './Main';
+</script>
+
+<style>
+  .warapper {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    background: var(--colors-app-bg);
+  }
+</style>
+
+<div class="warapper">
+  <Main>
+    <slot />
+  </Main>
+</div>
